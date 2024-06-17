222fdccbadb7d9723efc1fa5cfdbd2ddf2a65f85
WaniGaurav <gnwani95@gmail.com>
2024-06-17 20:37:52 +0530

    cart button created

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ function App() {
   return (
     <>
       <h1>Button Component</h1>
-      <div className="flex-group">
+      <div className="grid-group">
         <div className="column1">
-          <ButtonComponent heading="<Button />" class="graybtn" />
+          <ButtonComponent heading="<Button />" type="submit" class="graybtn" />
         </div>
         <div className="column2">
-          <ButtonComponent heading="<Button variant=”outline” />" class="outline" />
+          <ButtonComponent heading="<Button variant=”outline” />" type="submit" class="outline" />
         </div>
         <div className="column3">
-          <ButtonComponent heading="<Button variant=”text” />" class="text" />
+          <ButtonComponent heading="<Button variant=”text” />" type="submit" class="text" />
         </div>
         <div className="column4">
-          <ButtonComponent heading="<Button disableShadow />" />
+          <ButtonComponent heading="<Button disableShadow />" type="submit" class=""/>
         </div>
         <div className="column5">
-          <ButtonComponent heading="<Button disabled />" type="disabled" class="disabled" />
+          <ButtonComponent heading="<Button disabled />" disabled={true} class="disabled"/>
         </div>
         <div className="column6">
           Button
--- src/assets/ButtonComponent.jsx
+++ src/assets/ButtonComponent.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import './ButtonCompDesign.css' 
+import './ButtonCompDesign.css'
+import CartSvg from './images/shopping_cart.svg'
 
 const ButtonComponent = (desc) => {
+  let classes = ' '
+  if (desc.class) {
+    classes+=desc.class;
+  } else {
+    classes='';
+  }
   return (
     <>
       <div className="btn-info">
         <span className="btn-heading">{desc.heading}</span>
-        <button type={desc.type?`${desc.type}`:"submit"} className={`primary ${desc.class}`}>Default</button>
+        <button disabled={desc.disabled} type={desc.type} className={`primary${classes}`}>
+          <img src={CartSvg} alt="shopping cart image"/>
+          Default
+        </button>
       </div>
     </>
   );
